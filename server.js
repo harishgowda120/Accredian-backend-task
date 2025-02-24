1d25c6cfd7f927ce004861a0ebeb275b11580691
HARISH GOWDA N <160123683+harishgowda120@users.noreply.github.com>
2025-02-24 19:31:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors');  // Import CORS
+const cors = require('cors');  
 const connectDB = require('./config/db');
 const referralRoutes = require('./routes/referralRoutes');
 const dotenv = require('dotenv');
@@ -11,10 +11,14 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.use(cors({ origin: 'https://accredian-frontend-task-nj16.vercel.app', credentials: true })); // Enable CORS
-
+app.use(cors({ origin: 'https://accredian-frontend-task-nj16.vercel.app', credentials: true })); 
 app.use(express.json());
 
 app.use('/api', referralRoutes);
+
+// Simple GET method
+app.get('/', (req, res) => {
+    res.send('Server is running!');
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
